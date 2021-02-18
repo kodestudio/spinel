ba62c2f976aeda87905aa1b437da9976ef8809eb
quocthinhvo <quocthinhvo0@gmail.com>
2021-02-18 23:11:33 +0700

    Tạo setting.py nhằm lưu các thông số cài đặt

---
+++ src/setting.py
@@ -0,0 +1,16 @@
+"""
+Copyleft @kodestudio
+Author: Võ Quốc Thịnh (@quocthinhvo)
+File này nhằm mục đích lưu trữ một số thông số của Spinel
+"""
+
+# phiên bản
+version = '0.1'
+
+# Nhập các thông số cần thiết cho firebase, bao gồm:
+firebaseConfig = {
+        "apiKey": 'cc',
+        "authDomain": '',
+        "databaseURL": '',
+        "storageBucket": ''
+    }     
